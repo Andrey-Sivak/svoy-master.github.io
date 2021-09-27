1f650b9a98e4de8e50bf1081eeeb2da4adc8457b
a.sivak <riziy_bes@mail.ru>
2021-09-27 21:00:44 +0300

    init

---
+++ app/js/main.js
@@ -0,0 +1,58 @@
+'use strict';
+import * as $ from 'jquery';
+import './jquery.inputmask.min';
+
+
+const mobileWidth = 767;
+let isMobile = checkWidth();
+
+window.addEventListener('resize', () => {
+    isMobile = checkWidth();
+});
+
+setTimeout(() => {
+    if (!document.querySelector('.loader')) {
+        return;
+    }
+
+    const loader = document.querySelector('.loader');
+    if (loader.classList.contains('active')) {
+        loader.classList.remove('active');
+
+        setTimeout(() => {
+            loader.parentElement.removeChild(loader);
+        }, 300)
+    }
+}, 2500);
+
+window.addEventListener('load', function () {
+
+    (function loader() {
+        if (!document.querySelector('.loader')) {
+            return;
+        }
+
+        const loader = document.querySelector('.loader');
+
+        if (loader.classList.contains('active')) {
+            loader.classList.remove('active');
+        }
+
+        setTimeout(() => {
+            loader.parentElement.removeChild(loader);
+        }, 1500);
+
+    })();
+
+    (function inputMask() {
+        $(".phone-mask").inputmask({
+            mask:"+7(999)999-99-99",
+            "clearIncomplete": true
+        });
+    })();
+
+});
+
+function checkWidth() {
+    return mobileWidth > document.documentElement.clientWidth;
+}
